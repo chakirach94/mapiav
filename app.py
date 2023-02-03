8813abfc4a230876a761650db3979730a669e0df
chakirach94 <95524156+chakirach94@users.noreply.github.com>
2023-02-03 20:58:42 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,149 @@
+from flask import Flask, render_template
+import folium
+from folium.plugins import FloatImage
+from folium import plugins
+import ee
+import os
+import geemap.foliumap as geemap
+import ee
+from folium.plugins import FloatImage
+import numpy as np
+from folium import plugins
+from folium.plugins import HeatMap
+
+# Define a method for displaying Earth Engine image tiles on a folium map.
+def add_ee_layer(self, ee_object, vis_params, name):
+    
+    try:    
+        # display ee.Image()
+        if isinstance(ee_object, ee.image.Image):    
+            map_id_dict = ee.Image(ee_object).getMapId(vis_params)
+            folium.raster_layers.TileLayer(
+            tiles = map_id_dict['tile_fetcher'].url_format,
+            attr = 'Google Earth Engine',
+            name = name,
+            overlay = True,
+            control = True
+            ).add_to(self)
+
+        # display ee.ImageCollection()
+        elif isinstance(ee_object, ee.imagecollection.ImageCollection):    
+            ee_object_new = ee_object.mosaic()
+            map_id_dict = ee.Image(ee_object_new).getMapId(vis_params)
+            folium.raster_layers.TileLayer(
+            tiles = map_id_dict['tile_fetcher'].url_format,
+            attr = 'Google Earth Engine',
+            name = name,
+            overlay = True,
+            control = True
+            ).add_to(self)
+
+        # display ee.Geometry()
+        elif isinstance(ee_object, ee.geometry.Geometry):    
+            folium.GeoJson(
+            data = ee_object.getInfo(),
+            name = name,
+            overlay = True,
+            control = True
+        ).add_to(self)
+
+        # display ee.FeatureCollection()
+        elif isinstance(ee_object, ee.featurecollection.FeatureCollection):  
+            ee_object_new = ee.Image().paint(ee_object, 0, 2)
+            map_id_dict = ee.Image(ee_object_new).getMapId(vis_params)
+            folium.raster_layers.TileLayer(
+            tiles = map_id_dict['tile_fetcher'].url_format,
+            attr = 'Google Earth Engine',
+            name = name,
+            overlay = True,
+            control = True
+        ).add_to(self)
+    
+    except:
+        print("Could not display {}".format(name))
+
+
+# Add EE drawing method to folium.
+folium.Map.add_ee_layer = add_ee_layer
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('yourmap.html')
+
+@app.route('/map')
+def map():
+    my_map=geemap.Map(
+            basemap="HYBRID",
+            plugin_Draw=True,
+            Draw_export=True,
+            locate_control=True,
+            plugin_LatLngPopup=False,
+        )
+
+        
+
+    # Add custom base maps to folium
+    basemaps = {
+        'Google Maps': folium.TileLayer(
+            tiles = 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+            attr = 'Google',
+            name = 'Google Maps',
+            overlay = True,
+            control = True
+        ),
+        'Google Satellite': folium.TileLayer(
+            tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+            attr = 'Google',
+            name = 'Google Satellite',
+            overlay = True,
+            control = True
+        ),
+        'Google Terrain': folium.TileLayer(
+            tiles = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+            attr = 'Google',
+            name = 'Google Terrain',
+            overlay = True,
+            control = True
+        ),
+        'Google Satellite Hybrid': folium.TileLayer(
+            tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+            attr = 'Google',
+            name = 'Google Satellite',
+            overlay = True,
+            control = True
+        ),
+        'Esri Satellite': folium.TileLayer(
+            tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            attr = 'Esri',
+            name = 'Esri Satellite',
+            overlay = True,
+            control = True
+        )
+    }
+
+    # Add custom basemaps
+    basemaps['Google Satellite Hybrid'].add_to(my_map)
+    
+    data = (
+    np.random.normal(size=(100, 3)) *
+    np.array([[1, 1, 1]]) +
+    np.array([[48, 5, 1]])
+).tolist()
+    #fullscreen
+    plugins.Fullscreen().add_to(my_map)
+    HeatMap(data).add_to(folium.FeatureGroup(name='Heat Map').add_to(my_map))
+    folium.LayerControl().add_to(my_map)
+
+    
+
+    url = 'https://www.zupimages.net/up/22/39/h8au.png'
+    my_map.save('templates/map.html')
+    return render_template('yourmap.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
